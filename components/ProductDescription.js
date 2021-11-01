3e6e89578ed9cdf8cc5589b1dad13042b48f892a
Klemen Praznik <kp5207@student.uni-lj.si>
2021-11-01 19:41:12 +0100

    l9-done

---
+++ components/ProductDescription.js
@@ -0,0 +1,24 @@
+app.component("product-description", {
+    props: {
+        description: String
+    },
+    template:
+      /*html*/
+      ` 
+        <div class="product-desc">
+            {{ description }}
+        </div>
+      `,
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    },
+    computed: {
+      
+    },
+  });
+  
